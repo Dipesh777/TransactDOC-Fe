bf3faa20e41843e3fd22dfa8a6dcc52c376faeb8
DIPESH RAJPUT <dipesh777rajput@gmail.com>
2026-01-06 11:09:21 +0530

    Added style related to body

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -2,8 +2,7 @@ import { Outlet } from "react-router-dom";
 
 const AuthLayout = () => {
   return (
-    <div>
-      <h1>Auth Area</h1>
+    <div className="mt-8">
       <Outlet />
     </div>
   );
